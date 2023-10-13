5a7cf3eeeb6f98eda153c94566de6b8e816e5eb2
thedev25 <dilshodadhamjonov2515@gmail.com>
2023-10-14 01:15:36 +0500

    bugun dagi hatolar

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -13,33 +13,43 @@ const TodoList = () => {
   }
 
   const handleAdd = () => {
-    if (/\btoday\b/i.test(value)) {
+    if (value.trim().length == "") {
+      alert('input value is empty');
+    } 
+    else if (/\btoday\b/i.test(value)) {
       setTodos((prevTodos) => ({
         ...prevTodos,
         today: [...prevTodos.today, value],
       }));
     }
 
-    if (/\btomorrow\b/i.test(value)) {
+    else if (/\btomorrow\b/i.test(value)) {
       setTodos((prevTodos) => ({
         ...prevTodos,
         tomorrow: [...prevTodos.tomorrow, value],
       }));
     }
 
-    if (/\bthen\b/i.test(value)) {
+   else if (/\bthen\b/i.test(value)) {
       setTodos((prevTodos) => ({
         ...prevTodos,
         then: [...prevTodos.then, value],
       }));
     }
 
+    else {
+      setTodos((prevTodos) => ({
+        ...prevTodos,
+        today: [...prevTodos.today, value],
+      }));
+    }
     setValue("");
   }
 
   return (
     <div>
     <div  className='container' >
+        <div className="todolist">
         <div>
       <input
         type="text"
@@ -49,7 +59,7 @@ const TodoList = () => {
       <button onClick={handleAdd}>Add</button>
       </div>
       <div>
-        <h2>Today:</h2>
+        <h3>Today:</h3>
         <ul>
           {todos.today.map((item, index) => (
             <li key={index}>{item}</li>
@@ -57,7 +67,7 @@ const TodoList = () => {
         </ul>
       </div>
       <div>
-        <h2>Tomorrow:</h2>
+        <h3>Tomorrow:</h3>
         <ul>
           {todos.tomorrow.map((item, index) => (
             <li key={index}>{item}</li>
@@ -65,13 +75,14 @@ const TodoList = () => {
         </ul>
       </div>
       <div>
-        <h2>Then:</h2>
+        <h3>Then:</h3>
         <ul>
           {todos.then.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
       </div>
+        </div>
       </div>
     </div>
   );
